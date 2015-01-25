590ec14df086a931ea00bb0558b3c5971b14f6c9
bejavu <bejavu@gmail.com>
2015-01-25 18:48:40 +0200

    initial commit - only API functions

---
+++ index.js
@@ -0,0 +1,24 @@
+var bitcoin = require('bitcoinjs-lib');
+
+module.exports = Colu;
+
+function Colu(privateKey, cc, i) {
+  
+  
+}
+
+Colu.register(callback) {
+  
+}
+
+Colu.createRegistrationMessage(username) {
+  
+}
+
+Colu.createRegistrationQR(registrationMessage) {
+  
+}
+
+Colu.registerUser(registrationMessage) {
+  
+}
